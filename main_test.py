db5dfd4460b0e29c1c33d1b97f4356c9e9d03d2c
clubfullstack <165699267+clubfullstack@users.noreply.github.com>
2024-06-14 13:55:52 -0300

    Update main_test.py

--- main_test.py
+++ main_test.py
@@ -16,9 +16,6 @@
 from modulatedTestLibrary import *
 import threading
 from configGlobalVariables import *
-
-import socket
-
 ##################################################################################################################################################################################
 ##################################################################################################################################################################################
 # Configurações iniciais do teste
@@ -36,23 +33,3 @@ thread2 = threading.Thread(target = TestMaker)
 # Inicialização das threads
 thread1.start()
 thread2.start()
-
-# Conecte-se ao container QEMU na porta 5050 usando o nome do serviço definido no docker-compose.yml
-def main():
-    HOST = 'qemu-emulator'
-    PORT = 5050
-
-    try:
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.connect((HOST, PORT))
-            # Lógica de comunicação aqui
-            s.sendall(b'Hello, QEMU!')
-            data = s.recv(1024)
-            print('Received', repr(data))
-    except ConnectionRefusedError:
-        print("Could not connect to QEMU emulator. Is it running and accessible?")
-
-if __name__ == "__main__":
-    main()
-
-
